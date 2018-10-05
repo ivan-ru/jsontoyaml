ff703ee499b5055047321cc149d39f987277fc89
Ivan Rusli <ivan.ru@tnis.com>
2018-10-05 17:53:50 +0700

    change from json to yml, to only based on input parameters

--- main.go
+++ main.go
@@ -2,95 +2,56 @@ package main
 
 import (
 	"bufio"
-	"encoding/json"
 	"flag"
-	"io/ioutil"
 	"log"
 	"os"
 )
 
-// Deploy ...
-type Deploy struct {
-	APIVersion   string `json:"apiVersion"`
-	Domain       string `json:"domain"`
-	Name         string `json:"name"`
-	Image        string `json:"image"`
-	Environments []struct {
-		Name  string `json:"name"`
-		Value string `json:"value"`
-	} `json:"environments"`
-}
-
-// Service ...
-type Service struct {
-	APIVersion string `json:"apiVersion"`
-	Name       string `json:"name"`
-	Ports      []struct {
-		Port       string `json:"port"`
-		TargetPort string `json:"targetPort"`
-		NodePort   string `json:"nodePort"`
-		Name       string `json:"name"`
-	} `json:"ports"`
-}
+var (
+	domainName  = flag.String("domain", "", "Domain Name")
+	serviceName = flag.String("svc", "", "Service Name")
+	servicePort = flag.String("port", "", "Service Port")
+)
 
 func init() {
 	flag.Parse()
 }
 
 func main() {
-	var byteDeploy, byteService []byte
-	var dataDeploy Deploy
-	var dataService Service
 	var err error
-
-	byteDeploy, _ = ioutil.ReadFile("deploy.json")
-	byteService, _ = ioutil.ReadFile("service.json")
-
-	err = json.Unmarshal(byteDeploy, &dataDeploy)
-	if err != nil {
-		log.Fatal("error unmarshal json", err)
-		return
-	}
-	err = json.Unmarshal(byteService, &dataService)
-	if err != nil {
-		log.Fatal("error unmarshal json", err)
-		return
-	}
 	var deployYAMLString, serviceYAMLString string
 	newline := "\n"
 
 	// generate deploy.yaml string
-	deployYAMLString += "# this file for deployment " + dataDeploy.Name + newline
-	deployYAMLString += "apiVersion: " + dataDeploy.APIVersion + newline
+	deployYAMLString += "# this file for deployment " + *serviceName + newline
+	deployYAMLString += "apiVersion: extensions/v1beta1" + newline
 	deployYAMLString += "kind: Deployment" + newline
 	deployYAMLString += "metadata:" + newline
-	deployYAMLString += "  name: " + dataDeploy.Name + newline
+	deployYAMLString += "  name: " + *serviceName + newline
 	deployYAMLString += "  labels:" + newline
-	deployYAMLString += "    app: " + dataDeploy.Name + newline
+	deployYAMLString += "    app: " + *serviceName + newline
 	deployYAMLString += "spec:" + newline
 	deployYAMLString += "  replicas: 1" + newline
 	deployYAMLString += "  template:" + newline
 	deployYAMLString += "    metadata:" + newline
 	deployYAMLString += "      labels:" + newline
-	deployYAMLString += "        app: " + dataDeploy.Name + newline
+	deployYAMLString += "        app: " + *serviceName + newline
 	deployYAMLString += "    spec:" + newline
 	deployYAMLString += "      containers:" + newline
-	deployYAMLString += "      - name: " + dataDeploy.Name + newline
-	deployYAMLString += "        image: " + dataDeploy.Image + newline
+	deployYAMLString += "      - name: " + *serviceName + newline
+	deployYAMLString += "        image: [replace]" + newline
 	deployYAMLString += "        imagePullPolicy: Always" + newline
 	deployYAMLString += "        volumeMounts:" + newline
-	deployYAMLString += "        - mountPath: " + "/go/src/" + dataDeploy.Name + "/logs" + newline
-	deployYAMLString += "          name: " + dataDeploy.Name + "-logs" + newline
+	deployYAMLString += "        - mountPath: " + "/go/src/" + *serviceName + "/logs" + newline
+	deployYAMLString += "          name: " + *serviceName + "-logs" + newline
 	deployYAMLString += "        env:" + newline
-	for _, v := range dataDeploy.Environments {
-		deployYAMLString += "        - name: " + v.Name + newline
-		deployYAMLString += "          value: \"" + v.Value + "\"" + newline
-	}
+	deployYAMLString += "        - name: [replace]" + newline
+	deployYAMLString += "          value: \"[replace]\"" + newline
 	deployYAMLString += "      restartPolicy: Always" + newline
 	deployYAMLString += "      volumes:" + newline
-	deployYAMLString += "      - name: " + dataDeploy.Name + "-logs" + newline
+	deployYAMLString += "      - name: " + *serviceName + "-logs" + newline
 	deployYAMLString += "        hostPath:" + newline
-	deployYAMLString += "          path: /data/" + dataDeploy.Domain + "_" + dataDeploy.Name + "/logs" + newline
+	deployYAMLString += "          path: /data/" + *domainName + "_" + *serviceName + "/logs" + newline
 	deployYAMLString += "      imagePullSecrets:" + newline
 	deployYAMLString += "      - name: regsecret" + newline
 	deployYAMLString += "  strategy:" + newline
@@ -117,25 +78,29 @@ func main() {
 	wDeploy.Flush()
 
 	// generate service.yaml string
-	serviceYAMLString += "apiVersion: " + dataService.APIVersion + newline
+	serviceYAMLString += "apiVersion: extensions/v1beta1" + newline
 	serviceYAMLString += "kind: Service" + newline
 	serviceYAMLString += "metadata:" + newline
-	serviceYAMLString += "  name: " + dataService.Name + newline
+	serviceYAMLString += "  name: " + *serviceName + newline
 	serviceYAMLString += "  labels:" + newline
-	serviceYAMLString += "    app: " + dataService.Name + newline
+	serviceYAMLString += "    app: " + *serviceName + newline
 	serviceYAMLString += "spec:" + newline
 	serviceYAMLString += "  ports:" + newline
 
-	for _, v := range dataService.Ports {
-		serviceYAMLString += "  - port: " + v.Port + newline
-		serviceYAMLString += "    targetPort: " + v.TargetPort + newline
-		serviceYAMLString += "    nodePort: " + v.NodePort + newline
-		serviceYAMLString += "    name: " + v.Name + newline
-	}
+	// port HTTP
+	serviceYAMLString += "  - port: " + *servicePort + newline
+	serviceYAMLString += "    targetPort: " + *servicePort + newline
+	serviceYAMLString += "    # nodePort: [replace]" + newline
+	serviceYAMLString += "    name: app" + newline
+	// port GRPC
+	serviceYAMLString += "  - port: 5" + *servicePort + newline
+	serviceYAMLString += "    targetPort: 5" + *servicePort + newline
+	serviceYAMLString += "    # nodePort: [replace]" + newline
+	serviceYAMLString += "    name: grpc" + newline
 
 	serviceYAMLString += "  type: LoadBalancer" + newline
 	serviceYAMLString += "  selector:" + newline
-	serviceYAMLString += "    app: " + dataService.Name + newline
+	serviceYAMLString += "    app: " + *serviceName + newline
 
 	// write service
 	fService, err := os.Create("result/service.yaml")
